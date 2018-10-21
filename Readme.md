ff0767e6b0368bd78300e013f3a45e50436fed8b
akyan <james@gawn.uk>
2018-10-21 11:25:49 +0100

    Added CI and deployment configuration.

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 # Introduction
 
 A brief experiment using Angular to provide a simple web page supplying next leaving times for Thames Clippers from Greenwich peer (in the morning) and Tower peer (in the evening).
+
+![Build Status](https://codebuild.eu-west-2.amazonaws.com/badges?uuid=eyJlbmNyeXB0ZWREYXRhIjoiTzJ1Qm5jSFlQb1E4b3MxQmI4eldSck9LQ1NpdDZaQVR2QjVvRjhNb2JTdGJTbWtzenJvd3RBdXVYN2J5diszTWlQYW9SajFPdGQwLzAzRGNBaVJOenlNPSIsIml2UGFyYW1ldGVyU3BlYyI6ImF1ZEc1N0hJQVFOZk1uaHUiLCJtYXRlcmlhbFNldFNlcmlhbCI6MX0%3D&branch=master)
---
+++ main.tf
@@ -0,0 +1,71 @@
+terraform {
+  backend "s3" {
+    bucket = "ana-terraform-state"
+    key = "website-gawn-subdomain-cc/terraform.tfstate"
+    profile = "terraform"
+    region = "eu-west-2"
+  }
+}
+
+variable "profile" {
+  type = "string",
+  default = "terraform"
+}
+
+variable "region" {
+  type = "string",
+  default = "eu-west-2"
+}
+
+provider "aws" {
+  region = "${var.region}"
+  profile = "${var.profile}"
+}
+
+provider "aws" {
+  alias = "us-east-1"
+
+  region = "us-east-1"
+  profile = "${var.profile}"
+}
+
+variable "subdomain" {
+  type = "string"
+  default = "cc"
+}
+
+module "website" {
+  source = "github.com/jamesgawn/ana-terraform-shared.git/static-website/"
+
+  cert-domain = "*.gawn.uk"
+  site-name = "website-gawn-subdomain-${var.subdomain}"
+  site-domains = ["${var.subdomain}.gawn.uk", "${var.subdomain}.gawn.co.uk"]
+  root = "index.html"
+  github-repo = "https://github.com/jamesgawn/thames-clipper-next-depature-checker.git"
+}
+
+data "aws_route53_zone" "gawn_uk" {
+  name         = "gawn.uk."
+}
+
+module "gawn_uk" {
+  source = "github.com/jamesgawn/ana-terraform-shared.git/dns/dualstackaliasrecord"
+
+  zone_id = "${data.aws_route53_zone.gawn_uk.zone_id}"
+  name = "${var.subdomain}.${data.aws_route53_zone.gawn_uk.name}"
+  alias-target = "${module.website.domain_name}"
+  alias-hosted-zone-id = "${module.website.hosted_zone_id}"
+}
+
+data "aws_route53_zone" "gawn_co_uk" {
+  name         = "gawn.co.uk."
+}
+
+module "gawn_co_uk" {
+  source = "github.com/jamesgawn/ana-terraform-shared.git/dns/dualstackaliasrecord"
+
+  zone_id = "${data.aws_route53_zone.gawn_co_uk.zone_id}"
+  name = "${var.subdomain}.${data.aws_route53_zone.gawn_co_uk.name}"
+  alias-target = "${module.website.domain_name}"
+  alias-hosted-zone-id = "${module.website.hosted_zone_id}"
+}
